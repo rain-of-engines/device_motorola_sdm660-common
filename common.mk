9ddc075a5930b733cbc27ac13717c28bcd7f327a
Deivid21 <david.parra.ignacio@gmail.com>
2024-03-03 21:03:48 -0300

    MotoWaves: Initial import from nio-RRTS31.Q1-20-24-2
    * We already use the prebuilt libadspd.so libs in each variant, so
      that's all we're going to take away from this implementation.
    
    * Reference from https://gitlab.com/NemesisDevelopers/motorola/packages_apps_motowavesv2
    
    Change-Id: Idb0458567daae493404bb93db5136b64b4536cce
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>
    Signed-off-by: skullknight963 <viktor.9630@hotmail.com>

--- common.mk
+++ common.mk
@@ -287,6 +287,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     MotoActions
 
+# MotoWaves
+PRODUCT_PACKAGES += \
+    MotoWavesV2
+
 # netmgrd
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
